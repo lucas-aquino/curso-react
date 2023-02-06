dd6df9c13030b23e0e4b5262b1ade3b28c418b67
lucas-aquino <luckasakino@protonmail.com>
2023-02-06 14:00:17 -0300

    Initial commit

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import { Component } from 'react';
+import Dish, { Flag } from './dish';
+
+function Header(){
+  return (
+    <div>
+      <h1>
+        lucas-aquino &#128525;
+      </h1>
+      <Dish></Dish>
+    </div>
+  );
+}
+
+export default Header;
+
